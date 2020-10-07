a029a86862b63fc6e3e27139480386b2e7cc8a59
Amanda Otero <mandystar018@gmail.com>
2020-10-07 15:05:57 -0700

    Add createSpace method to the Board class

--- js/Board.js
+++ js/Board.js
@@ -1,7 +1,23 @@
 class Board {
   constructor () { //<=== No arguments this is because there is only one object for the board property (it will be the same board fot each game)
-    this.space = [];
-    this.row = 6;
+    this.rows = 6;
     this.columns = 7;
+    this.space = [];
+  }
+
+  createSpaces(){
+    const spaces = [];
+
+    for(let x = 0; x < this.columns; x++){
+      const column = [];
+
+      for(let y = 0; y < this.rows; y++){
+        const space = new Space(x , y);
+        column.push(space);
+      }
+      spaces.push(column);
+    }
+
+    return spaces;
   }
 }
--- js/Space.js
+++ js/Space.js
@@ -2,7 +2,7 @@ class Space {
   constructor (x,y) {
     this.x = x;
     this.y = y;
-    this.id = `space- ${x}-${y}`;
+    this.id = `space-${x}-${y}`;
     this.token = null;
   }
 
